f34b05939efe1e3a62bb1c947d3eeba502b48831
NicoBrandstetterr <83082508+NicoBrandstetterr@users.noreply.github.com>
2022-06-20 21:45:58 -0400

    Add files via upload

--- TerrorismApp/views.py
+++ TerrorismApp/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from terrorismapp.models import gName
 
 def seleccion_preguntas(request):
     return render(request,"principal.html")
 
-def Buscar(request):
-    mensaje="Esta representa la tabla respuesta a la query %r" %request.GET["prd"]
-    return HttpResponse(mensaje)
+
+def primeraQuery(request):
+    #query=gName.objects.raw('SELECT * FROM Tabla WHERE gname = "Hutu extremists"')
+    query=gName.objects.filter(gname='Hutu extremists')
+    return render(request, "1query.html",{"Tabla":query})
+
+
+def segundaQuery(request):
+    query=gName.objects.raw('SELECT * FROM Tabla WHERE gname = "Hutu extremists"')
+    #query=gName.objects.filter(gname='Hutu extremists')
+    return render(request, "1query.html",{"Tabla":query})
